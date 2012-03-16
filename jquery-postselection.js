abe90ea22b9ec599ceff6ba4c7e2d66c5dc1ab07
vlewin <vlewin@suse.de>
2012-03-16 16:11:06 +0100

    don't send ajax request on "mouseup" event

--- jquery-postselection.js
+++ jquery-postselection.js
@@ -2,44 +2,93 @@
   $.fn.postSelection = function(options) {
       var settings = $.extend( {
         'url' : '',
+        'message' : 'send selection'
+        
       }, options);
-    
+      
       get = function(){
-        if(window.getSelection){
-          return window.getSelection();
-        }else if(document.getSelection){
-          return document.getSelection();
-        }else if(document.selection){
-          return document.selection.createRange().text;
+        var selection = window.getSelection();
+        var range =  window.getSelection().getRangeAt(0);
+        
+        var $firstElement = $(window.getSelection().getRangeAt(0).startContainer.parentNode);
+        var $lastElement = $(window.getSelection().getRangeAt(0).endContainer.parentNode);
+        var $parentElement =   $(range.commonAncestorContainer);
+        
+        var selectedText = range.toString().replace(/(\r\n|\n|\r)/gm,"").replace(/[ \t\r]+/g," ");
+
+        var left = $firstElement.offset().left;
+        var top = $firstElement.offset().top;
+
+        var dialog = '<div id="selectionDialog"><a id="sendSelection" href="#" onclick="send()">' + settings.message + '</a><a id="removeSendSelection" href="#">x</a></div>';
+        
+        if(selectedText != '') {
+          if($parentElement.has("#selectionDialog").length == 0) { 
+            $parentElement.append(dialog);
+          }
+        } else {
+          $("#selectionDialog").remove();
         }
+        
+        
+        $("#selectionDialog").css({
+            "position":"absolute", 
+            "top": 0, 
+            "left": ($(window).width()/2)-80,
+            "width" : "160px",
+            "color" : "#fff",
+            "border" : "1px solid #222",
+            "border-radius": "0px 0px 5px 5px",
+            "text-shadow":"0px 0px 1px #000",
+            "padding" : "5px 0px",
+            "text-align" : "center",
+            "text-decoration" : "none",
+            "font-weight": "bold",
+            "background" : "#444"
+            
+        });        
+        
+        $("#sendSelection").css({
+            "color" : "#fff",
+            "text-shadow":"0px 0px 1px #000",
+            "text-decoration" : "none",
+            "margin-right" : "5px"
+        });
+        
+        $("#removeSendSelection").css({
+            "color" : "red",
+            "padding" : "0px 5px",
+            "text-decoration" : "none",
+            "margin-left" : "5px",
+            "background" : "#222"
+            
+        });
+        
+        return selectedText;
       }
       
-      send = function(){
-        var text = get();
-        if(text !=''){
-          try {
-            var text = text.toString();
-            
-            $.post(settings.url, {"data" : text}, function(data) {
-              console.log("success")
-            })
-            
-            .error(function(error) { console.log(error.status +  " " + error.statusText) });
-            
-          } catch(error)  {
-            console.log("error " + error)
-          }
+      remove = function() {
+        if(!$('body').has("#selectionDialog").length == 0) { 
+          $("#selectionDialog").remove()
         }
+      
+      }
+      
+      send = function(text){
+        var text = get();
+        $("#selectionDialog").remove()
+
+        if(text !=''){ $.post(settings.url, {"data" : text}, function(data) { }) }
       }
 
-      $(this).bind("mouseup", send)
+      $(this).bind("mouseup", get);
+      $("#removeSendSelection").live("click", remove);
       return true;
   };
 })( jQuery );
 
 
-
 $(function(){
-  var url = "<URL>";
-  $("<HTML ELEMENT>").postSelection({ url : url});
+  var host = "http://localhost:3000";
+  $("#ol").postSelection({ url : host});
 })
+
